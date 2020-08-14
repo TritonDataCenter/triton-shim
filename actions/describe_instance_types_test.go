aacfe633c87c88ab295b95990f078f11d4b350e0
Todd Whiteman <todd.whiteman@joyent.com>
2020-08-14 09:37:18 -0700

    tests: just use AWS testing (now that we have authentication in place)

--- actions/describe_instance_types_test.go
+++ actions/describe_instance_types_test.go
@@ -9,42 +9,12 @@
 package actions_test
 
 import (
-	"bytes"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/ec2"
 
-	"github.com/gin-gonic/gin"
-	"github.com/joyent/triton-shim/server"
 	"github.com/joyent/triton-shim/test"
 )
-
-func TestAccDescribeInstanceTypes(t *testing.T) {
-	// Switch to test mode so you don't get such noisy output
-	gin.SetMode(gin.TestMode)
-
-	engine := server.Setup()
-
-	req, err := http.NewRequest(http.MethodPost, "/", bytes.NewBufferString("Action=DescribeInstanceTypes"))
-	if err != nil {
-		t.Fatalf("Couldn't create request: %v\n", err)
-	}
-
-	// Create a response recorder so we can inspect the response
-	w := httptest.NewRecorder()
-
-	// Perform the request
-	engine.ServeHTTP(w, req)
-
-	// log.Printf("[ERROR]: Response body: %s", w.Body.String())
-	// Check the response
-	if w.Code != http.StatusOK {
-		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
-	}
-
-}
 
 func TestAccAWSDescribeInstanceTypes(t *testing.T) {
 	test.GetEC2Svc(t, func(ec2Svc *ec2.EC2) {
